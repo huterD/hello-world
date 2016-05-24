53e5c805f79f93b625cd5ded21f091a24fdcca27
huterD <hac.in.hora@gmail.com>
2016-05-24 16:28:23 -0500

    Create daily_programmer.py

---
+++ daily_programmer.py
@@ -0,0 +1,12 @@
+def ordinal_formatter(i):
+    suffixes = ["th", "st", "nd", "rd", "th", "th", "th", "th", "th", "th"]
+    if (i // 10) % 10 == 1:
+        return str(i) + 'th'
+    else:
+        return str(i) + suffixes[i % 10]
+
+
+def ordinal(n, size):
+    return list(map(ordinal_formatter, [i for i in range(1,size+1) if i != n]))
+
+print(ordinal(5, 37))
