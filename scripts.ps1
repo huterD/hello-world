7b5bd77f2667d2bf7a41f7d2de04ee1705b2b29c
huterD <hac.in.hora@gmail.com>
2016-07-14 16:38:57 -0500

    Create scripts.ps1
    Scripts for powershell for useful tasks.

---
+++ scripts.ps1
@@ -0,0 +1,1 @@
+Get-ChildItem -Recurse c:\directory\to\scan\ | ForEach-Object {$_ | add-member -name "Owner" -membertype noteproperty -value (get-acl $_.fullname).owner -passthru} | Sort-Object fullname | Select FullName,CreationTime,LastWriteTime,Length,Owner | Export-Csv -Force -NoTypeInformation c:\folder\to\directory.csv
